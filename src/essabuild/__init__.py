378398460eec4dd70e228b74bd4cd8ff932c1c7d
Maciej <sppmacd@pm.me>
2023-02-20 20:03:05 +0100

    Remove some exports that shouldn't be used directly

--- src/essabuild/__init__.py
+++ src/essabuild/__init__.py
@@ -2,5 +2,4 @@ from essabuild.BuildConfig import BuildConfig
 from essabuild.BuildSystem import project
 from essabuild.Config import config
 from essabuild.Project import Project
-from essabuild.Target import Target, TargetType
 import essabuild.Utils as utils
